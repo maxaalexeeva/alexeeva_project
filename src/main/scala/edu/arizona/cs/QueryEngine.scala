ebe2e53020ebf9525e6ceaddd5153c1cc20d28ea
Masha Alexeeva <maxaalexeeva@gmail.com>
2019-04-27 11:30:16 -0700

    seems to lemmatize but not efficient

---
+++ src/main/scala/edu/arizona/cs/QueryEngine.scala
@@ -0,0 +1,236 @@
+
+package edu.arizona.cs
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import java.io._
+
+import org.apache.lucene.search.similarities.Similarity
+import org.apache.lucene.search.similarities.ClassicSimilarity
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.StringField
+import org.apache.lucene.document.TextField
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.index.IndexReader
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.queryparser.classic.ParseException
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.Query
+import org.apache.lucene.search.ScoreDoc
+import org.apache.lucene.search.TopDocs
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.RAMDirectory
+
+import scala.collection.mutable.ListBuffer
+import java.io.File
+import java.io.IOException
+import java.util.Scanner
+
+import org.clulab.processors.shallownlp.ShallowNLPProcessor
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConversions._
+import scala.io.Source
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.collection.{immutable, mutable}
+
+object QAEngine {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Hi")
+    val qa = new QAEngine
+    qa.train("wiki")
+
+//    try {
+//      val fileName: String = "input.txt"
+//      println("********Welcome to  Homework 3!")
+//      val query13a: List[String] = List("information", "retrieval")
+//      val objQueryEngine: QueryEngine = new QueryEngine(fileName)
+//      val ans2: ListBuffer[ResultClass] = objQueryEngine.runQ1(query13a)
+//      println(":value of ans2 is" + ans2)
+//
+//      for (x <- ans2) {
+//        println("value of x is " + x.DocName.get("docid"))
+//      }
+//    } catch {
+//      case ex: Exception => println(ex.getMessage)
+//
+//    }
+  }
+
+}
+
+class QAEngine() {
+  protected lazy val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+  def train(collection: String): Unit = {
+
+    val files = getListOfFiles("./src/main/resources/wiki")
+
+
+    val data = for {
+      file <- files
+      line <- Source.fromFile(file).getLines()
+    } yield line
+
+    val proc = new ShallowNLPProcessor()
+
+    //val titleBody = mutable.Map[String, String]()
+    val fw = new FileWriter("test.txt", true)
+
+    val allData = data.mkString(" ").split("\\[\\[") //split on [[ to separate entries
+    for (line <- allData if line.length >0) {
+      println(allData.indexOf(line))
+
+      val splitLine = line.split("\\]\\]") //split on ]] to separate title from body
+      fw.write(splitLine.head + "\t")
+      val textToLemmatize = splitLine.tail.mkString(" ").split("\\. ").filter(m => m.length>5) //break body up into sentences
+      //textToLemmatize.foreach(m => println(m + "\n"))
+      val doc = proc.annotateFromSentences(textToLemmatize.headOption) //lemmatize
+
+      //println(doc.sentences.foreach(sent => sent.lemmas.mkString(" ") + ". " + "\n"))
+      //fw.write(splitLine.head + "\t" + doc.sentences.foreach(sent => sent.lemmas.mkString(" ") + ". " + "\n"))
+//      val bodyLemmatized = for {
+//        sent <- doc.sentences
+//        lemmas <- sent.lemmas
+//      } yield lemmas.mkString(" ") + "."
+      //      println(splitLine.head + "\n")
+      //      println(bodyLemmatized.mkString(" "))
+
+      for {
+        sent <- doc.sentences
+        lemmas <- sent.lemmas
+        } fw.write(lemmas.mkString(" ") + "." + "\n")
+//            println(splitLine.head + "\n")
+//            println(bodyLemmatized.mkString(" "))
+
+      //fw.write(splitLine.head + "\t" + bodyLemmatized.mkString(" ") + "\n")
+
+    }
+
+
+    fw.close()
+
+    println("tada")
+  }
+
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+}
+//  def runQ1(query: List[String]): ListBuffer[ResultClass] = {
+//    val ans = returnDummyResults()
+//    return ans;
+//  }
+//
+//  def runQ13a(query: List[String]): ListBuffer[ResultClass] = {
+//    val ans = returnDummyResults()
+//    return ans;
+//  }
+//
+//  def runQ13b(query: List[String]): ListBuffer[ResultClass] = {
+//    val ans = new ListBuffer[ResultClass]()
+//    return ans;
+//  }
+//
+//  def runQ13c(query: List[String]): ListBuffer[ResultClass] = {
+//    val ans = returnDummyResults()
+//    return ans;
+//  }
+//
+//
+//  private def returnDummyResults(): ListBuffer[ResultClass] = {
+//    var doc_score_list = new ListBuffer[ResultClass]()
+//
+//    for (i <- 0.until(2)) {
+//      val doc: Document = new Document()
+//      doc.add(new TextField("title", "", Field.Store.YES))
+//      doc.add(
+//        new StringField("docid",
+//          "Doc" + java.lang.Integer.toString(i + 1),
+//          Field.Store.YES))
+//      val objResultClass: ResultClass = new ResultClass()
+//      objResultClass.DocName = doc
+//      doc_score_list += (objResultClass)
+//    }
+//    return doc_score_list
+//  }
+
+
+
+
+
+//      for (sent<- doc.sentences) {
+//        sent.lemmas.foreach(lem => println("=>" + lem.mkString(" ")))
+//      }
+//      println(splitLine.head + "\n")
+//      println(splitLine.tail.mkString(" ") + "\n")
+//      println(splitLine.head.mkString(" "))
+//      println("\n")
+//      println("-->" + splitLine.tail.mkString(" "))
+//      println("\n")
+//      val doc = proc.annotateFromSentences(splitLine.tail.mkString(" ").split(". ")) //(splitLine.tail.mkString(" "))
+//      for (d <- doc.sentences) {
+//        d.lemmas.foreach(lemma => lemma.mkString(" "))
+//      }
+//      for {
+//              sent <- doc.sentences
+//              sentTok = sent.lemmas.head.mkString(" ").split(" ")
+//              word <- sentTok
+//            } println(word)
+
+//      val uniLemmas = for {
+//        sent <- doc.sentences
+//        sentTok = sent.lemmas.head.mkString(" ").split(" ")
+//        word <- sentTok
+//      } yield word
+//      //for (word <- uniLemmas) println(word + "\n")
+//      titleBody += (splitLine.head -> uniLemmas.mkString(" "))
+
+//for (tb <- titleBody) println(tb._1 + "\n")
+//for (tb <- titleBody) println(tb._2 + "\n")
+//
+//    for (item <- titleBody) {
+//      println(item._1 + "\n")
+//      print(item._2 + "\n")
+//
+//    }
+
+//    val unigrams = for {
+//      sent <- doc.sentences
+//      sentTok = sent.lemmas.head.mkString(" ").split(" ")
+//      docId = sentTok(0)
+//      word <- sentTok
+//      if (sentTok.indexOf(word) != 0)
+//
+//    } yield word
+
+
+//    val text = "hello i was doing homework"
+//    val proc = new ShallowNLPProcessor()
+//    val annotated = proc.annotate(text)
+//    val lemmas = for {
+//      doc <- annotated.sentences
+//      lem <- doc.lemmas
+//
+//    } yield lem.mkString(" ")
+//
+//    for (x <- lemmas) println(x)
+
+//
+//    println(allData)
+//    for (d <- data) println(d + "\n")
+
+//    val source = Source.fromResource(collection)
+//    println(source.length)
+//    val docs = for (line <- source.getLines()) yield line
+//    println("docs done")
+//    val listDocs = docs.toList
+//    println(listDocs)
